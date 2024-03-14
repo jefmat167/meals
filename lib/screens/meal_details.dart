d99998a7157856eedbf9e9ebcb0ed2a8c9f34afe
Isaac Matthew <issac@fisshboneandlestr.com>
2024-03-14 17:08:23 +0100

    add multiscreen animation

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:meals/models/meal.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -45,11 +46,14 @@ class MealDetailsScreen extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover
+              ),
             ),
             const SizedBox(height: 14,),
             Text(
